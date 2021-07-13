16fa545ac783dd4e359873d9f9d79bad6edd1049
= <=>
2021-07-13 22:55:05 +0200

    FMT

--- nbterm/nbterm.py
+++ nbterm/nbterm.py
@@ -10,6 +10,7 @@ from .notebook import Notebook
 
 from jupyter_client.kernelspec import KernelSpecManager
 
+
 def list_kernels_callback(value: bool):
     if value:
         kernelSpecs = KernelSpecManager().find_kernel_specs()
